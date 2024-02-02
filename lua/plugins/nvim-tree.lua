8320e02f577bbb50c6b997859991ef8bd3dce60b
Nikolai Peisong Li <coopakamy@gmail.com>
2024-02-02 22:19:16 +0800

    make nvim-tree follow cwd of the buffer

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,11 +1,16 @@
 return {
   "nvim-tree/nvim-tree.lua",
   version = "*",
-  lazy = false,
+  event = "VeryLazy",
   dependencies = {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
-    require("nvim-tree").setup({})
+    require("nvim-tree").setup({
+      update_focused_file = {
+        enable = true,
+        update_cwd = true,
+      },
+    })
   end,
 }
