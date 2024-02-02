b70f994823623db3ecbf171afea673f54a5b392a
Nikolai Peisong Li <coopakamy@gmail.com>
2024-02-02 20:36:03 +0800

    edited starting UI and keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,5 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 local keymap = vim.keymap
-local opts = { noremap = true, silent = true }
 -- Select all
 keymap.set("n", "<C-a>", "gg<S-v>G")
--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -2,8 +2,7 @@ return {
 
   { "nvimdev/dashboard-nvim", enabled = false },
   { "echasnovski/mini.starter", enabled = false },
-  -- Dashboard. This runs when neovim starts, and is what displays
-  -- the "LAZYVIM" banner.
+  -- Customized dashboard with only features necessary
   {
     "goolord/alpha-nvim",
     event = "VimEnter",
@@ -30,15 +29,9 @@ return {
       dashboard.section.header.val = vim.split(logo, "\n")
       -- stylua: ignore
       dashboard.section.buttons.val = {
-        dashboard.button("f", " " .. " Find file",       "<cmd> Telescope find_files <cr>"),
-        dashboard.button("n", " " .. " New file",        "<cmd> ene <BAR> startinsert <cr>"),
         dashboard.button("r", " " .. " Recent files",    "<cmd> Telescope oldfiles <cr>"),
-        dashboard.button("g", " " .. " Find text",       "<cmd> Telescope live_grep <cr>"),
-        dashboard.button("c", " " .. " Config",          "<cmd> lua require('lazyvim.util').telescope.config_files()() <cr>"),
         dashboard.button("s", " " .. " Restore Session", [[<cmd> lua require("persistence").load() <cr>]]),
-        dashboard.button("x", " " .. " Lazy Extras",     "<cmd> LazyExtras <cr>"),
-        dashboard.button("l", "󰒲 " .. " Lazy",            "<cmd> Lazy <cr>"),
-        dashboard.button("q", " " .. " Quit",            "<cmd> qa <cr>"),
+       dashboard.button("q", " " .. " Quit",            "<cmd> qa <cr>"),
       }
       for _, button in ipairs(dashboard.section.buttons.val) do
         button.opts.hl = "AlphaButtons"
