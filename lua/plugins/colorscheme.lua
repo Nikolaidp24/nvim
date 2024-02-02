b9701d4e2a614eea8415cf91fecafeccc31a6b19
Nikolai Peisong Li <coopakamy@gmail.com>
2024-02-02 19:16:21 +0800

    modified kanagawa settings

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,6 +3,10 @@ return {
     "rebelot/kanagawa.nvim",
     priority = 1000,
     config = function()
+      require("kanagawa").setup({
+        transparent = true,
+        -- dimInactive = true,
+      })
       require("kanagawa").load("dragon")
       vim.cmd([[colorscheme kanagawa]])
     end,
