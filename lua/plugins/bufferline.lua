7a46f63ac1c02b1e524e452a85654d957c33ab19
Nikolai Peisong Li <coopakamy@gmail.com>
2024-02-03 22:30:53 +0800

    improved bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -7,10 +7,36 @@ return {
   },
   opts = {
     options = {
-      mode = "slant",
-      -- separator_style = "slant",
+      mode = "buffers",
       show_buffer_close_icons = false,
       show_close_icon = false,
+      diagnostics = "nvim_lsp",
+      always_show_bufferline = true,
+      diagnostics_indicator = function(_, _, diag)
+        local icons = require("lazyvim.config").icons.diagnostics
+        local ret = (diag.error and icons.Error .. diag.error .. " " or "")
+          .. (diag.warning and icons.Warn .. diag.warning or "")
+        return vim.trim(ret)
+      end,
+      offsets = {
+        {
+          filetype = "NvimTree",
+          text = "File Explorer",
+          text_align = "center",
+          highlight = "Directory",
+          separator = true,
+        },
+      },
     },
   },
+  config = function(_, opts)
+    require("bufferline").setup(opts)
+    vim.api.nvim_create_autocmd("BufAdd", {
+      callback = function()
+        vim.schedule(function()
+          pcall(nvim_bufferline)
+        end)
+      end,
+    })
+  end,
 }
