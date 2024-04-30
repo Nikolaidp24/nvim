6be982c5daade6cc84ee37c43b84c3650d98cf35
Nikolaidp24 <coopakamy@gmail.com>
2024-04-30 14:59:22 +0800

    resume ruby provider after setting proxy rules

--- init.lua
+++ init.lua
@@ -1,7 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 vim.g.python3_host_prog = "~/py3_cmp/bin/python"
--- vim.g.ruby_host_prog = "~/.rbenv/versions/3.1.2/bin/neovim-ruby-host"
-vim.g.loaded_ruby_provider = 0
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.g.loaded_perl_provider = 0
