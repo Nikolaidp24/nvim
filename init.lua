1d31cfaa15a05c589c72e3a9f2fc8912fd5d462c
Nikolaidp24 <coopakamy@gmail.com>
2024-05-20 15:44:10 +0800

    changed mkdp style

--- init.lua
+++ init.lua
@@ -3,4 +3,5 @@ vim.g.python3_host_prog = "~/py3_cmp/bin/python"
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.g.loaded_perl_provider = 0
+vim.g.mkdp_markdown_css = "/usr/local/lib/node_modules/markdown-air/css/air.css"
 require("config.lazy")
