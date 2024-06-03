68e2496daa3e8678eedbb76d7c8d5617d8217fa5
Nikolaidp24 <coopakamy@gmail.com>
2024-06-03 15:38:59 +0800

    remove conflict keymap and update

--- init.lua
+++ init.lua
@@ -3,5 +3,6 @@ vim.g.python3_host_prog = "/usr/bin/python3"
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
 vim.g.mkdp_markdown_css = "/usr/local/lib/node_modules/markdown-air/css/air.css"
 require("config.lazy")
