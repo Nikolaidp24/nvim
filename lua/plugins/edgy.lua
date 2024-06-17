e984ed74803a9699b8c83bcd896a704b3243b6ed
Nikolaidp24 <coopakamy@gmail.com>
2024-06-17 14:22:21 +0800

    update and fix edgy plugin conflict with Extras

--- lua/plugins/edgy.lua
+++ lua/plugins/edgy.lua
@@ -33,16 +33,6 @@ return {
             return vim.api.nvim_win_get_config(win).relative == ""
           end,
         },
-        -- { ft = "qf", title = "QuickFix" },
-        -- {
-        --   ft = "help",
-        --   size = { height = 20 },
-        --   -- don't open help files in edgy that we're editing
-        --   filter = function(buf)
-        --     return vim.bo[buf].buftype == "help"
-        --   end,
-        -- },
-        -- { title = "Neotest Output", ft = "neotest-output-panel", size = { height = 15 } },
       },
       keys = {},
     }
